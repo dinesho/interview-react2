f5008dda99edf7654341292d64f1720a1436d8fb
Dinesh Don <dinesh.opatha@gmail.com>
2019-12-19 09:06:59 -0500

    Add util class to manage date/time manipulation

---
+++ activity-tracker/src/Util/Util.js
@@ -0,0 +1,38 @@
+
+export  const getDuration = function(start,end = -1){
+    let duration;
+
+    if(end < 0)
+    {
+        let date = new Date();
+        let now = date.getTime();
+
+        duration = now - start;
+    }
+    else
+        {
+            duration = end - start;
+        }
+
+    //convert duration into seconds
+    duration = Math.floor(duration/1000);
+
+    // converting ms duration in to H:M:S
+    let hours = Math.floor(duration / 3600);
+    let minutes = Math.floor((duration - hours*3600) / 60);
+    let seconds = duration - (hours * 3600) - (minutes * 60);
+
+    return hours+" : "+minutes+" : "+seconds;
+};
+
+export const getDateTime = function (time) {
+    if(time < 0)
+    {
+        return "In Progress";
+    }
+    
+    let date = new Date(parseInt(time));
+    return date.getHours() +":"+date.getMinutes()+":"+date.getSeconds();
+};
+
+export default getDuration;
